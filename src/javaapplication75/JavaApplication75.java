0d48918fb4bbd17bd4dff5aa2ff46bbab6675cfe
unknown <marioadalfiquezadamarcelino@alumno.ieselrincon.es>
2026-03-04 15:50:31 +0000

    Estructura inicial:Clase Tarea

---
+++ src/javaapplication75/JavaApplication75.java
@@ -0,0 +1,7 @@
+package javaapplication75;
+public class JavaApplication75 {
+    public static void main(String[] args) {
+
+    }
+    
+}
---
+++ src/javaapplication75/Tarea.java
@@ -0,0 +1,36 @@
+package javaapplication75;
+
+/**
+ *Clase representa una tarea del sistema 
+ * 
+ * @author Mario
+ * 
+ */
+public class Tarea {
+
+  
+    private String descripcion;
+    private boolean completada;
+
+   /**
+    * El constructor de la tarea
+    * 
+    * @param descripción Descripcion de la tarea
+    */
+    public Tarea(String descripcion) {
+        this.descripcion = descripcion;
+        this.completada = false;
+    }
+    
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
